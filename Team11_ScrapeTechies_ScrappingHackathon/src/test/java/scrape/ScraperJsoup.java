5129c7a616a810bf299271d18e7524aee5af2ab7
Madhuchhanda Sahu <madhuchhanda.sahu03@gmail.com>
2024-07-12 11:02:35 -0400

    preparation method

---
+++ Team11_ScrapeTechies_ScrappingHackathon/src/test/java/scrape/ScraperJsoup.java
@@ -0,0 +1,68 @@
+package scrape;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import pojo.RecipeData;
+
+public class ScraperJsoup {
+	public List<RecipeData> extractRecipeData() {
+
+		try {
+			Document recipeDoc = Jsoup.connect("https://www.tarladalal.com/RecipeAtoZ.aspx").get();
+
+			List<Element> elements = recipeDoc.select("div[class=rcc_recipecard]");
+
+			// System.out.println("Element" + elements);
+			
+			List<RecipeData> dataList = new ArrayList<>();
+
+			for (Element e : elements) {
+
+				RecipeData data = new RecipeData();
+
+				data.setRecipeId(e.attribute("id").getValue());
+				data.setRecipeUrl(e.select("span[class=rcc_recipename]").select("a[href]").attr("href"));
+				data.setRecipeName(e.select("span[class=rcc_recipename]").select("a[href]").text());
+
+				dataList.add(data);
+			}
+
+			for (RecipeData rd : dataList) {
+
+				List<String> ings = new ArrayList<>();
+				List<String> prepMethod = new ArrayList<>();
+				Document ingredientsDoc = Jsoup.connect("https://www.tarladalal.com/" + rd.getRecipeUrl()).get();
+
+				for (Element e : ingredientsDoc.select("span[itemprop=recipeIngredient]")) {
+
+					ings.add(e.select("a").text());
+
+				}
+				for (Element p : ingredientsDoc.select("div[id=recipe_small_steps]")) {
+
+					prepMethod.add(p.select("li").text());
+
+				}
+
+				rd.setPrepTime(ingredientsDoc.select("Time[itemprop=prepTime]").text());
+				rd.setCookTime(ingredientsDoc.select("Time[itemprop=cookTime]").text());
+
+				rd.setIngredients(ings);
+				rd.setPreparationMethod(prepMethod);
+			}
+
+			return dataList;
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
