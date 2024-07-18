b6aab94ae39ca101adac7bf78017c942e74e25d1
MeenakshiMuthuraman <144947917+MeenakshiMuthuraman@users.noreply.github.com>
2024-07-18 15:06:38 -0400

    Removed unwanted codes

--- src/test/java/report/JSONReportGenerator.java
+++ src/test/java/report/JSONReportGenerator.java
@@ -2,23 +2,60 @@ package report;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
 import pojo.RecipeData;
 
 public class JSONReportGenerator {
+	
+	private static final Logger log = LogManager.getLogger(JSONReportGenerator.class);
 
-    public static void generateReport(List<RecipeData> data) {
+    public static void generateReport(List<RecipeData> data, String fileName) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(data);
 
-        try (FileWriter writer = new FileWriter("report.json")) {
+        try (FileWriter writer = new FileWriter(fileName)) {
             writer.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("JSON report generated successfully.");
+        log.info("JSON report generated successfully.");
     }
+    
+    public static void generateErrorReport(Map<String,RecipeData> data, String fileName) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(data);
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info("JSON ERROR report generated successfully.");
+    }
+    
+    public static List<RecipeData> getRecipeDataList(String fileName){
+    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    	try {
+			return Arrays.asList(gson.fromJson(new InputStreamReader(JSONReportGenerator.class.getResourceAsStream("/RecipeDB/" + fileName)), RecipeData[].class));
+		} catch (JsonSyntaxException | JsonIOException e) {
+			e.printStackTrace();
+		}
+    	return null;
+    	
+    }
+    
 }
